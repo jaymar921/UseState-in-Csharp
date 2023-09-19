db63bf3d7973820145231b80bdf783c58f2d4a7b
JayMar921 <jayharronabejar@gmail.com>
2023-09-19 21:47:37 +0800

    Add project files.

---
+++ Classes/State.cs
@@ -0,0 +1,31 @@
+﻿namespace Csharp_Deconstructing_Tuples.Classes
+{
+    public class State<T>
+    {
+        private T? v;
+
+        public State(T t)
+        {
+            v = t;
+        }
+
+        public State()
+        {
+        }
+
+        public (Func<T?>, Action<T>) CreateState(T? val)
+        {
+            v = val;
+            return (Get, Set);
+        }
+
+        private void Set(T t)
+        {
+            v = t;
+        }
+        private T? Get()
+        {
+            return v;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Csharp_Deconstructing_Tuples.Classes;
+
+namespace Csharp_Deconstructing_Tuples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Creating State just inspired by ReactJS hehehe, it's ReactCS XD
+                
+                Creating a state
+                
+                     getter    setter
+                var (Func<T>, Funct<T>) = new State<T>().CreateState();
+
+                example:
+                var (name, setName) = new State<string>().CreateState();
+
+                setName("hello World"); // < -- setting the name value to "hello World"
+                Console.WriteLine(name()) // < -- getting the name value by invoking the getter method
+                Console.WriteLine(name.Invoke()) // < -- another way of invoking the getter method
+            */
+
+            var (name, setName) = new State<string>().CreateState("jay");
+            var (age, setAge) = new State<int>().CreateState(22);
+
+            Console.WriteLine("Original Name: "+name());
+            setName("jayharron");
+            Console.WriteLine("New Name: " + name());
+
+            Console.WriteLine("Original Age: " + age());
+            setAge(23);
+            Console.WriteLine("New Age: " + age());
+        }
+    }
+}
